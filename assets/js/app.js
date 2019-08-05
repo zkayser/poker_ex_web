6cd482596e650331e47bb26101a9eb5cbe276e08
Kayser <zkayser@gmail.com>
2019-08-05 08:42:47 -0400

    Create Root component

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,15 +9,9 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import reducers from './reducers'
-import App from './components/App'
+import { Root } from './components/Root'
 
 const store = createStore(reducers)
 
 render(
-  <Provider store={store}>
-    <Router>
-      <Route path="/" component={App} />
-    </Router>
-  </Provider>,
-  document.querySelector('.app')
-)
+  <Root store={store} />, document.querySelector('.app'))
